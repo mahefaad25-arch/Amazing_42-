1e2011b4eb1cb2f0b148becb5877cf7265ce9fde
mahefaad25-arch <mahefaad25@gmail.com>
2026-06-09 14:16:04 +0300

    confirmer

--- test_pars.py
+++ test_pars.py
@@ -7,7 +7,7 @@
 #   By: bramahef <bramahef@student.42antananarivo.   +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/06/09 11:19:53 by bramahef            #+#    #+#            #
-#   Updated: 2026/06/09 13:28:46 by bramahef           ###   ########.fr      #
+#   Updated: 2026/06/09 14:15:43 by bramahef           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
@@ -35,7 +35,6 @@ def config_parser(filename: str) -> Dict[str, Any]:
                 value = value.strip()
                 
                 try:
-                    # Conversion des types
                     if key in ["WIDTH", "HEIGHT"]:
                         config[key] = int(value)
                     elif key in ["ENTRY", "EXIT"]:
@@ -44,10 +43,9 @@ def config_parser(filename: str) -> Dict[str, Any]:
                     elif key == "PERFECT":
                         config[key] = (value == "True")
                     else:
-                        # Permet de stocker d'autres clés optionnelles (seed, etc.)
                         config[key] = value
                 except ValueError:
-                    print(f"Erreur de valeur ligne {line_num}: Impossible de convertir '{value}' pour la clé '{key}'.")
+                    print(f"Erreur de valeur ligne {line_num}: Impossible de convertir")
                     sys.exit(1)
                     
     except FileNotFoundError:
@@ -57,7 +55,6 @@ def config_parser(filename: str) -> Dict[str, Any]:
         print(f"Erreur: Droits insuffisants pour lire le fichier '{filename}'.")
         sys.exit(1)
         
-    # Vérification des clés obligatoires requises par le sujet
     mandatory_keys = ["WIDTH", "HEIGHT", "ENTRY", "EXIT", "PERFECT", "OUTPUT_FILE"]
     for m_key in mandatory_keys:
         if m_key not in config:
@@ -66,7 +63,6 @@ def config_parser(filename: str) -> Dict[str, Any]:
             
     return config
 
-# Exemple d'utilisation sécurisé
 if __name__ == "__main__":
     # Récupère le fichier passé en argument comme demandé par le sujet
     if len(sys.argv) != 2:
